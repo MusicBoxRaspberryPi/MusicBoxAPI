1f436b3de40fb9ba88b12e07b67080792eef6f47
Tim <herew26@gmail.com>
2023-12-06 18:47:42 +0300

    chore: remove old files

--- src/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .track import Track
-from .device import Device
--- src/models/track.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Track:
-    id: str
-
-    def __post_init__(self):
-        self.uri = f"spotify:track:{self.id}"
