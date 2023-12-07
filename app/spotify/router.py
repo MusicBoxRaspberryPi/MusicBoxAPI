7f59064286bca464472dd461e89dfe23decf974f
Tim <herew26@gmail.com>
2023-12-07 22:36:00 +0300

    feat: add current, next and previous device endpoints

--- app/spotify/router.py
+++ app/spotify/router.py
@@ -2,15 +2,63 @@ from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends
 
 from app.container import Container
-from app.spotify.schemas import Device
+from app.spotify.schemas import Device, CurrentDevice
 from app.spotify.service import SpotifyService
 
 spotify_router = APIRouter()
 
 
-@spotify_router.get("/devices")
+@spotify_router.get("/device")
 @inject
-def get_devices(
-        spotify_service: SpotifyService = Depends(Provide[Container.spotify_service]),
-) -> list[Device]:
-    return spotify_service.refresh_devices()
+def get_current_device(
+        spotify_service: SpotifyService = Depends(Provide[Container.spotify_service])
+) -> CurrentDevice | dict:
+    spotify_service.refresh_devices()
+    current_device = spotify_service.get_current_device()
+
+    if current_device is None:
+        return {}
+
+    return CurrentDevice(
+        device=current_device,
+        index=spotify_service.get_current_device_index(),
+        total=spotify_service.get_devices_count()
+    )
+
+
+@spotify_router.get("/device/next")
+@inject
+def next_device(
+        spotify_service: SpotifyService = Depends(Provide[Container.spotify_service])
+) -> CurrentDevice | dict:
+    spotify_service.refresh_devices()
+    spotify_service.next_device()
+    current_device = spotify_service.get_current_device()
+
+    if current_device is None:
+        return {}
+
+    return CurrentDevice(
+        device=current_device,
+        index=spotify_service.get_current_device_index(),
+        total=spotify_service.get_devices_count()
+    )
+
+
+@spotify_router.get("/device/previous")
+@inject
+def previous_device(
+        spotify_service: SpotifyService = Depends(Provide[Container.spotify_service])
+) -> CurrentDevice | dict:
+    spotify_service.refresh_devices()
+    spotify_service.previous_device()
+    current_device = spotify_service.get_current_device()
+
+    if current_device is None:
+        return {}
+
+    return CurrentDevice(
+        device=current_device,
+        index=spotify_service.get_current_device_index(),
+        total=spotify_service.get_devices_count()
+    )
